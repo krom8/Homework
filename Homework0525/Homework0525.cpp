4e572609220c0c37948ba047ee249a1f45d3e9f3
kiloh <krom51423@gmail.com>
2023-05-26 09:05:04 +0900

    0525 질문사항

--- Homework0525/Homework0525.cpp
+++ Homework0525/Homework0525.cpp
@@ -21,3 +21,7 @@ int main()
 	FZone.Fight(NewPlayer, NewMonster);
 
 }
+
+
+// class에 관해서만 다룬거 같은데 함수나 변수같은 경우를 분리해서 쓰고 싶으면 어떡해야하냐
+// 그냥 class 만들듯이 만들면 어디에서 온건지 구분이 안가는데 그래서 namespace가 필요한거이고 이게 어느정도 스탠다드가 맞나????
